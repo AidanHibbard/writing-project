b1f55235cfb56cc7f9593fd604908ddec7350c77
Aidan Hibbard <aidanchibbard@gmail.com>
2022-05-12 15:11:20 -0700

    Final

---
+++ assets/sources.html.dd8a5b49.js
@@ -0,0 +1,1 @@
+const e={key:"v-b4bb04cc",path:"/sources.html",title:"",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"Sources Cited",slug:"sources-cited",children:[]},{level:2,title:"Media",slug:"media",children:[]}],git:{updatedTime:1652376602e3,contributors:[{name:"Aidan Hibbard",email:"aidanchibbard@gmail.com",commits:3}]},filePathRelative:"sources.md"};export{e as data};
